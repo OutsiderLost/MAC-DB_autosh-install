85cc5a6c1bfe0c6e94d64068bd4bb3764116d0b4
OutsiderLost <87493359+OutsiderLost@users.noreply.github.com>
2022-01-11 22:58:49 +0100

    Add files via upload

---
+++ MAC-DB_autosh-install.sh
@@ -0,0 +1,43 @@
+echo "For windows -> http://www.nirsoft.net/utils/mac_address_lookup_find.html"
+echo " "
+# mkdir MACview-Windows wget && http://www.nirsoft.net/utils/macaddressview.zip && mv macaddressview.zip MACview-Windows/macaddressview.zip
+
+echo "Download MAC database"
+echo " "
+
+locate oui.txt
+echo " "
+
+wget http://standards-oui.ieee.org/oui/oui.txt
+
+sleep 3
+
+echo " "
+echo "Create -> oui.sh"
+# -----------------------------------
+echo '#!/bin/bash' > oui-script.txt
+echo '   ' >> oui-script.txt
+echo 'MAC="$(echo $1 | sed "s/ //g" | sed "s/-//g" | sed "s/://g" | cut -c1-6)";' | sed "s/\"/\'/2;s/\"/\'/2;s/\"/\'/2;s/\"/\'/2;s/\"/\'/2;s/\"/\'/2" >> oui-script.txt
+echo '  ' >> oui-script.txt
+echo 'result="$(grep -i -A 4 ^$MAC ./oui.txt)";' >> oui-script.txt
+echo '  ' >> oui-script.txt
+echo 'if [ "$result" ]; then' >> oui-script.txt
+echo '    echo "For the MAC $1 the following information is found:"' >> oui-script.txt
+echo '    echo "$result"' >> oui-script.txt
+echo 'else' >> oui-script.txt
+echo '    echo "MAC $1 is not found in the database."' >> oui-script.txt
+echo 'fi' >> oui-script.txt
+# -----------------------------------
+sleep 3
+
+mv oui-script.txt oui.sh
+
+sleep 2
+
+echo " "
+echo "use -> bash oui.sh MAC"
+echo " "
+echo "example -> bash oui.sh 50:46:5D:6E:8C:20"
+echo " "
+
+bash oui.sh 50:46:5D:6E:8C:20
